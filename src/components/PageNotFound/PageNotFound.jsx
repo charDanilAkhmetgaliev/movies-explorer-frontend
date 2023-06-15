5f14846539d3ae99e9d3191c00253b9e8cca19b4
charDanilAkhmetgaliev <akhm3tgal1ev@yandex.ru>
2023-06-15 17:35:19 +0300

    feature: Ð to layuotµpage not found

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import './PageNotFound.css';
+import { NotFoundPageConfig } from '../../config.style';
+import './PageNotFound.scss';
 
 function PageNotFound() {
   return (
     <div className="not-found">
-      <h3 className="not-found__title">
-        <span>404</span>
-        {' '}
-        - Страница не найдена
-      </h3>
-      <p className="not-found__text">
-        Ой, здесь ничего нет
-      </p>
-      <Link className="button button_type_to-main" to="/">Назад</Link>
+      <h1 className="not-found__header">{NotFoundPageConfig.headerContent}</h1>
+      <p className="not-found__sub-header">{NotFoundPageConfig.subheaderContent}</p>
     </div>
   );
 }
---
+++ src/config.style.js
@@ -0,0 +1,14 @@
+const NotFoundPageConfig = {
+  headerContent: 404,
+  subheaderContent: 'Страница не найдена',
+  backLinkContent: 'Назад',
+};
+
+const MainConfig = {
+
+};
+
+export {
+  NotFoundPageConfig,
+  MainConfig,
+};
