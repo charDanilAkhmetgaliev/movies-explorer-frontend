41d373879597529d2dee853994f25bd08570fc44
charDanilAkhmetgaliev <akhm3tgal1ev@yandex.ru>
2023-06-15 12:25:12 +0300

    feature: создать репозитрий, файловую структуру, роÑмаршруты(роуты)

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" />
+        <Route path="/movies" />
+        <Route path="/saved-movies" />
+        <Route path="/profile" />
+        <Route path="/signin" />
+        <Route path="/signup" />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
