d7078f3636d925d5c23f925804d662f22c7f96da
charDanilAkhmetgaliev <akhm3tgal1ev@yandex.ru>
2023-06-20 19:02:45 +0300

    feature: to do layout and style Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className="header" />
+    <div className="header">
+      <div className="header__logo" />
+      <div className="header__burger-menu" />
+    </div>
   );
 }
 
--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { NotFoundPageConfig } from '../../config.style';
+import Header from '../Header/Header';
 
 function PageNotFound() {
   const navigate = useNavigate();
@@ -8,11 +9,15 @@ function PageNotFound() {
   const onBackHandler = () => navigate(-1);
 
   return (
-    <div className="not-found">
-      <h1 className="not-found__header">{NotFoundPageConfig.headerText}</h1>
-      <p className="not-found__sub-header">{NotFoundPageConfig.subheaderText}</p>
-      <button className="not-found__back-button" type="button" onClick={onBackHandler}>{NotFoundPageConfig.backButtonText}</button>
-    </div>
+    <>
+      <Header />
+      <div className="not-found">
+        <h1 className="not-found__header">{NotFoundPageConfig.headerText}</h1>
+        <p className="not-found__sub-header">{NotFoundPageConfig.subheaderText}</p>
+        <button className="not-found__back-button" type="button" onClick={onBackHandler}>{NotFoundPageConfig.backButtonText}</button>
+      </div>
+    </>
+
   );
 }
 
