049e9168cb73d37d9901f44cc70e517402633945
charDanilAkhmetgaliev <akhm3tgal1ev@yandex.ru>
2023-06-20 17:53:38 +0300

    feature: finish page not found layout

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { NotFoundPageConfig } from '../../config.style';
 
 function PageNotFound() {
+  const navigate = useNavigate();
+
+  const onBackHandler = () => navigate(-1);
+
   return (
     <div className="not-found">
-      <div className="not-found__info">
-        <h1 className="not-found__header">{NotFoundPageConfig.headerText}</h1>
-        <p className="not-found__sub-header">{NotFoundPageConfig.subheaderText}</p>
-      </div>
-      <button className="not-found__back-button" type="button">{NotFoundPageConfig.backButtonText}</button>
+      <h1 className="not-found__header">{NotFoundPageConfig.headerText}</h1>
+      <p className="not-found__sub-header">{NotFoundPageConfig.subheaderText}</p>
+      <button className="not-found__back-button" type="button" onClick={onBackHandler}>{NotFoundPageConfig.backButtonText}</button>
     </div>
   );
 }
