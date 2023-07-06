2dee28ab8f8e7d7836507d4b7352040d8fcf2610
charDanilAkhmetgaliev <akhm3tgal1ev@yandex.ru>
2023-07-06 17:21:04 +0300

    feature: to do layout for about project section

--- src/components/Main/AboutProject/AboutProject.jsx
+++ src/components/Main/AboutProject/AboutProject.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import { MainPageConfig } from '../../../config.style';
 
 function AboutProject() {
+  const aboutProjectData = MainPageConfig.aboutProject.itemList;
+  const { tableData } = MainPageConfig.aboutProject;
+
   return (
     <div className="about-project">
       <h2 className="about-project__title">{MainPageConfig.aboutProject.titleText}</h2>
       <div className="about-project__separating-line" />
       <ul className="about-project__list">
-        <li className="about-project__list-item">
-          <h3 className="about-project__item-header">{MainPageConfig.aboutProject.itemList.firstItem.headerText}</h3>
-          <p className="about-project__item-info">{MainPageConfig.aboutProject.itemList.firstItem.infoText}</p>
-        </li>
-        <li className="about-project__list-item">
-          <h3 className="about-project__item-header">{MainPageConfig.aboutProject.itemList.secondItem.headerText}</h3>
-          <p className="about-project__item-info">{MainPageConfig.aboutProject.itemList.secondItem.infoText}</p>
-        </li>
+        { aboutProjectData.map((item) => (
+          <li className="about-project__list-item">
+            <h3 className="about-project__item-title">{item.title}</h3>
+            <p className="about-project__item-info">{item.info}</p>
+          </li>
+        ))}
       </ul>
+      <table className="about-project__table">
+        {
+          tableData.map((row) => (
+            <tr className="about-project__table-row">
+              {
+                row.map((cell) => (
+                  <td className="about-project__table-cell">{cell}</td>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </table>
     </div>
   );
 }
