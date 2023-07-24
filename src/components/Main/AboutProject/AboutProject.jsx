84797c59cf9b998c65d9c916023b5530cdf7d18a
Danil Akhmetgaliev <akhm3tgal1ev@yandex.ru>
2023-07-24 17:04:28 +0300

    feature: complete styles to ABoutProject section

--- src/components/Main/AboutProject/AboutProject.jsx
+++ src/components/Main/AboutProject/AboutProject.jsx
@@ -4,33 +4,34 @@ import { MainPageConfig } from '../../../config.style';
 function AboutProject() {
   const aboutProjectData = MainPageConfig.aboutProject.itemList;
   const { tableData } = MainPageConfig.aboutProject;
-
   return (
     <section className="about-project">
       <div className="about-project__title-container">
         <h2 className="about-project__title">{MainPageConfig.aboutProject.titleText}</h2>
-        <hr className="about-project__separating-line" />
+        <div className="about-project__separating-line" />
       </div>
       <ul className="about-project__list">
         { aboutProjectData.map((item) => (
-          <li className="about-project__list-item">
+          <li key={Math.floor(Math.random() * 1000 + 1)} className="about-project__list-item">
             <h3 className="about-project__item-title">{item.title}</h3>
             <p className="about-project__item-info">{item.info}</p>
           </li>
         ))}
       </ul>
       <table className="about-project__table">
-        {
-          tableData.map((row) => (
-            <tr className="about-project__table-row">
-              {
-                row.map((cell) => (
-                  <td className="about-project__table-cell">{cell}</td>
-                ))
-              }
-            </tr>
-          ))
-        }
+        <tbody className="about-project__table-body">
+          {
+            tableData.map((row) => (
+              <tr key={Math.floor(Math.random() * 100 + 1)} className="about-project__table-row">
+                {
+                  row.map((cell) => (
+                    <td key={Math.floor(Math.random() * 100 + 1)} className="about-project__table-cell">{cell}</td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     </section>
   );
